dd419ffee15de7c3efe000e54f423a3a7995b35e
KBuild <qwer7995@gmail.com>
2020-04-22 23:38:27 +0900

    send mail when there are any not soldout merchandise

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const puppeteer = require('puppeteer');
 const process = require('process');
+const nodemailer = require('nodemailer');
+
+const mailopt = {
+  user: process.env.MAIL_USER,
+  pass: process.env.MAIL_PASS,
+  from: process.env.MAIL_USER,
+  to: process.env.MAIL_TO,
+}
+
+async function sendMail(message) {
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: mailopt.user,
+      pass: mailopt.pass,
+    },
+  });
+
+  try {
+    const res = await transport.sendMail({
+      from: mailopt.from,
+      to: mailopt.to,
+      subject: "Go Buy Switch!!!!!",
+      text: message,
+    });
+
+    console.info("mail ok!");
+  } catch {
+    console.error("mail fail");
+  }
+}
 
 async function instanceBrowser() {
   if (process.env.RUNAS === "WSL") {
     // for using this instance, please add Chromium binary path of Windows to runtime path
     return puppeteer.launch({
       executablePath: 'chrome.exe',
-      headless: false,
+      headless: true,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
@@ -22,12 +53,23 @@ async function instanceBrowser() {
 
 (async () => {
   const browser = await instanceBrowser();
-  const page = await browser.newPage();
-  await page.goto('https://store.nintendo.co.jp/search.html?sshow=1&category=console_accessory&sub_category=console&sort=release_new&from=shelf&page=1');
-  await page.waitFor('.o_c-item-card');
-  const list = await page.$$eval('.o_c-item-card', els => els.map(el => el.textContent.trim()));
-  const soldoutList = list.map(str => str.includes('品切れ'));
+  try {
+    const page = await browser.newPage();
+    await page.goto('https://store.nintendo.co.jp/search.html?sshow=1&category=console_accessory&sub_category=console&sort=release_new&from=shelf&page=1');
+    await page.waitFor('.o_c-item-card', {
+      timeout: 30000,
+    });
+    const list = await page.$$eval('.o_c-item-card', els => els.map(el => el.textContent.trim()));
+    const notsold = list.filter(str => !str.includes('品切れ'));
 
-  console.dir(list, soldoutList);
-  await browser.close();
+    if (notsold.length > 0) {
+      await sendMail(notsold.join("\n"));
+    }
+    console.info("program end");
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await browser.close();
+    process.exit();
+  }
 })();
